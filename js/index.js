75d69991ed8f11eff616ccac198159df070971c4
programmershakib <programmershakibinfo@gmail.com>
2024-09-24 00:41:57 +0600

    js function done

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ document.getElementById('btn-1')
 
 
             const historyList = document.createElement('div');
-            historyList.classList.add('border', 'rounded-lg');
+            historyList.classList.add('border-2', 'rounded-lg');
 
             historyList.innerHTML = `
                 <p class="text-secondary text-xl font-bold my-5 mx-10">${donatedMoney} Taka is Donated for Donate for Flood at Noakhali, Bangladesh</p>
@@ -44,7 +44,7 @@ document.getElementById('btn-2')
 
 
             const historyList = document.createElement('div');
-            historyList.classList.add('border', 'rounded-lg');
+            historyList.classList.add('border-2', 'rounded-lg');
 
             historyList.innerHTML = `
                 <p class="text-secondary text-xl font-bold my-5 mx-10">${donatedMoney} Taka is Donated for Donate for Flood Relief in Feni,Bangladesh</p>
@@ -76,10 +76,74 @@ document.getElementById('btn-3')
 
 
             const historyList = document.createElement('div');
-            historyList.classList.add('border', 'rounded-lg');
+            historyList.classList.add('border-2', 'rounded-lg');
 
             historyList.innerHTML = `
                 <p class="text-secondary text-xl font-bold my-5 mx-10">${donatedMoney} Taka is Donated for Aid for Injured in the Quota Movement</p>
+                <p class="text-third mb-5 mx-10">Date: ${new Date()}</p>
+            `;
+            const historyContainer = element('history-list');
+            historyContainer.appendChild(historyList);
+
+            const modal = document.getElementById('my-modal');
+            modal.showModal();
+        }
+        else{
+            alert('Please Valid Input')
+        }
+});
+
+
+document.getElementById('btn-4')
+    .addEventListener('click', function(){
+        const availableBalance = inputNumber('balance');
+        const donatedMoney = inputValue('input-4');
+        const balance = inputNumber('balance-4');
+
+        if(donatedMoney > 0){
+            const availableMoney = availableBalance - donatedMoney;
+            element('balance').innerText = availableMoney;
+
+            element('balance-4').innerText = balance + donatedMoney;
+
+
+            const historyList = document.createElement('div');
+            historyList.classList.add('border-2', 'rounded-lg');
+
+            historyList.innerHTML = `
+                <p class="text-secondary text-xl font-bold my-5 mx-10">${donatedMoney} Taka is Donated for Child Labor in Bangladesh</p>
+                <p class="text-third mb-5 mx-10">Date: ${new Date()}</p>
+            `;
+            const historyContainer = element('history-list');
+            historyContainer.appendChild(historyList);
+
+            const modal = document.getElementById('my-modal');
+            modal.showModal();
+        }
+        else{
+            alert('Please Valid Input')
+        }
+});
+
+
+document.getElementById('btn-5')
+    .addEventListener('click', function(){
+        const availableBalance = inputNumber('balance');
+        const donatedMoney = inputValue('input-5');
+        const balance = inputNumber('balance-5');
+
+        if(donatedMoney > 0){
+            const availableMoney = availableBalance - donatedMoney;
+            element('balance').innerText = availableMoney;
+
+            element('balance-5').innerText = balance + donatedMoney;
+
+
+            const historyList = document.createElement('div');
+            historyList.classList.add('border-2', 'rounded-lg');
+
+            historyList.innerHTML = `
+                <p class="text-secondary text-xl font-bold my-5 mx-10">${donatedMoney} Taka is Donated for poor people in Bangladesh</p>
                 <p class="text-third mb-5 mx-10">Date: ${new Date()}</p>
             `;
             const historyContainer = element('history-list');
@@ -121,6 +185,9 @@ donationBtn.addEventListener('click', function(){
 
     const card = document.getElementById('card');
     card.classList.remove('hidden');
+
+    const footerBg = document.getElementById('footer-bg');
+    footerBg.classList.remove('hidden');
 });
 
 document.getElementById('blog-btn').addEventListener('click', function(){
