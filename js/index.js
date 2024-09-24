6edf457b00dd60278972120dabe534dfabd6d0cc
programmershakib <programmershakibinfo@gmail.com>
2024-09-24 12:43:36 +0600

    common function done

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ document.getElementById('btn-1')
 
             element('balance-1').innerText = balance + donatedMoney;
 
-
             const historyList = document.createElement('div');
             historyList.classList.add('border-2', 'rounded-lg');
 
@@ -18,13 +17,11 @@ document.getElementById('btn-1')
                 <p class="text-secondary text-xl font-bold my-5 mx-10">${donatedMoney} Taka is Donated for Donate for Flood at Noakhali, Bangladesh</p>
                 <p class="text-third mb-5 mx-10">Date: ${new Date()}</p>
             `;
-            const historyContainer = element('history-list');
-            historyContainer.appendChild(historyList);
+            element('history-list').appendChild(historyList);
 
-            const modal = document.getElementById('my-modal');
-            modal.showModal();
+            element('my-modal').showModal();
 
-            document.getElementById('input-1').value = '';
+            element('input-1').value = '';
         }
         else{
             alert('Please Type Valid Input')
@@ -44,7 +41,6 @@ document.getElementById('btn-2')
 
             element('balance-2').innerText = balance + donatedMoney;
 
-
             const historyList = document.createElement('div');
             historyList.classList.add('border-2', 'rounded-lg');
 
@@ -52,13 +48,11 @@ document.getElementById('btn-2')
                 <p class="text-secondary text-xl font-bold my-5 mx-10">${donatedMoney} Taka is Donated for Donate for Flood Relief in Feni,Bangladesh</p>
                 <p class="text-third mb-5 mx-10">Date: ${new Date()}</p>
             `;
-            const historyContainer = element('history-list');
-            historyContainer.appendChild(historyList);
+            element('history-list').appendChild(historyList);
 
-            const modal = document.getElementById('my-modal');
-            modal.showModal();
+            element('my-modal').showModal();
 
-            document.getElementById('input-2').value = '';
+            element('input-2').value = '';
         }
         else{
             alert('Please Type Valid Input')
@@ -78,7 +72,6 @@ document.getElementById('btn-3')
 
             element('balance-3').innerText = balance + donatedMoney;
 
-
             const historyList = document.createElement('div');
             historyList.classList.add('border-2', 'rounded-lg');
 
@@ -86,13 +79,11 @@ document.getElementById('btn-3')
                 <p class="text-secondary text-xl font-bold my-5 mx-10">${donatedMoney} Taka is Donated for Aid for Injured in the Quota Movement</p>
                 <p class="text-third mb-5 mx-10">Date: ${new Date()}</p>
             `;
-            const historyContainer = element('history-list');
-            historyContainer.appendChild(historyList);
+            element('history-list').appendChild(historyList);
 
-            const modal = document.getElementById('my-modal');
-            modal.showModal();
+            element('my-modal').showModal();
 
-            document.getElementById('input-3').value = '';
+            element('input-3').value = '';
         }
         else{
             alert('Please Type Valid Input')
@@ -112,7 +103,6 @@ document.getElementById('btn-4')
 
             element('balance-4').innerText = balance + donatedMoney;
 
-
             const historyList = document.createElement('div');
             historyList.classList.add('border-2', 'rounded-lg');
 
@@ -120,13 +110,11 @@ document.getElementById('btn-4')
                 <p class="text-secondary text-xl font-bold my-5 mx-10">${donatedMoney} Taka is Donated for Donate for Child Labor in Bangladesh</p>
                 <p class="text-third mb-5 mx-10">Date: ${new Date()}</p>
             `;
-            const historyContainer = element('history-list');
-            historyContainer.appendChild(historyList);
+            element('history-list').appendChild(historyList);
 
-            const modal = document.getElementById('my-modal');
-            modal.showModal();
+            element('my-modal').showModal();
 
-            document.getElementById('input-4').value = '';
+            element('input-4').value = '';
         }
         else{
             alert('Please Type Valid Input')
@@ -146,7 +134,6 @@ document.getElementById('btn-5')
 
             element('balance-5').innerText = balance + donatedMoney;
 
-
             const historyList = document.createElement('div');
             historyList.classList.add('border-2', 'rounded-lg');
 
@@ -154,13 +141,11 @@ document.getElementById('btn-5')
                 <p class="text-secondary text-xl font-bold my-5 mx-10">${donatedMoney} Taka is Donated for Donate for poor people in Bangladesh</p>
                 <p class="text-third mb-5 mx-10">Date: ${new Date()}</p>
             `;
-            const historyContainer = element('history-list');
-            historyContainer.appendChild(historyList);
+            element('history-list').appendChild(historyList);
 
-            const modal = document.getElementById('my-modal');
-            modal.showModal();
+            element('my-modal').showModal();
 
-            document.getElementById('input-5').value = '';
+            element('input-5').value = '';
         }
         else{
             alert('Please Type Valid Input')
@@ -168,36 +153,33 @@ document.getElementById('btn-5')
 });
 
 
-const historyBtn = document.getElementById('history-btn');
-const donationBtn = document.getElementById('donation-btn');
+const historyBtn = element('history-btn');
+const donationBtn = element('donation-btn');
 
 historyBtn.addEventListener('click', function(){  
     historyBtn.classList.add('bg-primary');
+    historyBtn.classList.remove('border-black', 'border-2');
     donationBtn.classList.remove('bg-primary');
-    donationBtn.classList.add('border-solid', 'border-2');
+    donationBtn.classList.add('border-2', 'border-black');
 
-    const card = document.getElementById('card');
-    card.classList.add('hidden');
+    element('history-list').classList.remove('hidden');
 
-    const historyList = document.getElementById('history-list');
-    historyList.classList.remove('hidden');
+    element('card').classList.add('hidden');
 
-    const footerBg = document.getElementById('footer-bg');
-    footerBg.classList.add('hidden');
+    element('footer-bg').classList.add('hidden');
 });
 
 donationBtn.addEventListener('click', function(){  
     donationBtn.classList.add('bg-primary');
+    donationBtn.classList.remove('border-black', 'border-2');
     historyBtn.classList.remove('bg-primary');
+    historyBtn.classList.add('border-2', 'border-black');
 
-    const historyList = document.getElementById('history-list');
-    historyList.classList.add('hidden');
+    element('history-list').classList.add('hidden');
 
-    const card = document.getElementById('card');
-    card.classList.remove('hidden');
+    element('card').classList.remove('hidden');
 
-    const footerBg = document.getElementById('footer-bg');
-    footerBg.classList.remove('hidden');
+    element('footer-bg').classList.remove('hidden');
 });
 
 document.getElementById('blog-btn').addEventListener('click', function(){
